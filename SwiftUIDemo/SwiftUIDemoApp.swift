2753732a921fa7e86d6b2ec49bd25ab9b57136b1
蘇健豪 <jenhausu@osensetech.com>
2022-06-10 09:55:36 +0800

    feat: add SwiftUIDemo Target

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by 蘇健豪 on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
